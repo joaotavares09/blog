e4bbcafb8874f95cad6f2d179cdf97a6a69776fb
joaotavares09 <jentavares09@gmail.com>
2020-01-14 17:25:18 -0300

    atualizando post

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import Post
-from django.utls import timezone
+from django.utils import timezone
 
 def post_list(request):
 	posts = Post.objects.filter(published_date__lte=timezone.now())
